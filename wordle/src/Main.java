583714f402d38c26774fb6795add7c713afb4718
NeilElvirsson <151514156+NeilElvirsson@users.noreply.github.com>
2023-11-24 11:16:40 +0100

    NeileBest!

--- wordle/src/Main.java
+++ wordle/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
     JFrame WordleJFrame = new WordleJFrame();
         WordleJFrame.setVisible(true);
+        //Hejsan Alrik Neil här!
     }
 }
